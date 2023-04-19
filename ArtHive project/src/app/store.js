034ae338aef3abc4ad854b0a85db88ad75dce02a
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-19 13:03:28 +0200

    feat: created store.js file in app folder for redux storage

---
+++ ArtHive project/src/app/store.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk]
+});
