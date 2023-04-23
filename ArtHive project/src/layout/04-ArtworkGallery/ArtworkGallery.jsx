51835882c30dca0b5a1578b1c7fdce7c30ae8f9d
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-23 11:26:22 +0200

    :feat: created artworkSlice to save artworkSelected details in Redux. Added spinner.

--- ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
+++ ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
@@ -2,22 +2,32 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { bringAllArtworks } from '../../services/apiCalls'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Spinner } from 'react-bootstrap'
 
 import './ArtworkGallery.css'
-import { Navigate, useNavigate } from 'react-router'
+import { useNavigate } from 'react-router'
 import { NavBar } from '../../components/Navbar/NavBar'
+import { useDispatch } from 'react-redux'
+import { addChoosenArtwork } from '../artworkSlice'
 
 export const ArtworkGallery = () => {
   const [allArtworks, setAllArtworks] = useState([])
+  const [loading, setLoading] = useState(true);
+
   // const [postHovered, setPostHovered] = useState(false)
 
+  const dispatch = useDispatch();
   const navigate = useNavigate()
   
+  const userCredentialsRdx = useSelector(userData)
+
+
   useEffect(() => {
     if (allArtworks.length === 0) {
       bringAllArtworks()
         .then((result) => {
+            setLoading(false);
+
           if (result.data.data.length === 0) {
             return
           }
@@ -27,9 +37,37 @@ export const ArtworkGallery = () => {
     }
   }, [allArtworks])
 
+  const artworkSelected = (artwork) => {
+    dispatch(addChoosenArtwork({ choosenArtwork: artwork }));
+    setTimeout(() => {
+      navigate("/artwork-detail");
+    }, 1000);
+  };
+
+
+
+  if (loading) {
+    return (
+        <>
+    <NavBar/>
+
+      <div className="spinnerDesign d-flex justify-content-center align-items-center flex-column">
+        <div>
+          {" "}
+          <Spinner animation="border" variant="primary" />
+        </div>
+        <div>
+          {" "}
+          <h4>Loading...</h4>
+        </div>
+      </div>
+      </>
+    );
+} else if (allArtworks.length > 0) {
   return (
     <>
     <NavBar/>
+
     <Container fluid>
       <h2>All artworks:</h2>
       <Row className="homeSection1">
@@ -46,7 +84,7 @@ export const ArtworkGallery = () => {
                     <img
                       // onMouseEnter={() => setPostHovered(true)}
                       // onMouseLeave={() => setPostHovered(false)}
-                      onClick={() => Navigate(`/artwork-detail/${artwork.id}`)}
+                      onClick={() => artworkSelected(artwork)}
                       className="ImgDesign"
                       src={`http://localhost:3000/static/${artwork.image_url}`}
                     />
