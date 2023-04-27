5b0ae16c670509d4cc0688cffa01a4eb5cbff82c
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-27 12:58:46 +0200

    feat: button save/saved working now. it updates when clicking and add or updates favorite field from logged user

--- ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
+++ ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { addFavorite, bringAllArtworks, bringMyUserArtworks, updateFavorite } from '../../services/apiCalls'
+import {
+  addFavorite,
+  bringAllArtworks,
+  bringAllArtworksAsUser,
+  bringMyUserArtworks,
+  updateFavorite,
+} from '../../services/apiCalls'
 import { Col, Container, Row, Spinner } from 'react-bootstrap'
 
 import './ArtworkGallery.css'
@@ -11,44 +17,37 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addChoosenArtwork, artworkData } from '../artworkSlice'
 import { userData } from '../userSlice'
 import { SpinnerComponent } from '../../components/SpinnerComponent/SpinnerComponent'
-import { addChoosenUserArtwork, userArtworkData } from '../userArtworkSlice'
 
 export const ArtworkGallery = () => {
+  const userCredentialsRdx = useSelector(userData)
+
   const [allArtworks, setAllArtworks] = useState([])
   const [loading, setLoading] = useState(true)
   const [savingPost, setSavingPost] = useState(false)
   const [postHovered, setPostHovered] = useState(false)
   const [myUserArtwork, setMyUserArtwork] = useState([])
- 
-  // const alreadySaved = 
+  const [isAlreadySaved, setIsAlreadySaved] = useState({})
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const userCredentialsRdx = useSelector(userData)
-  const userArtworkDetailsRdx = useSelector(userArtworkData)
-
   useEffect(() => {
-  
     if (myUserArtwork.length === 0) {
-        bringMyUserArtworks(userCredentialsRdx.credentials.token)
-          .then((result) => {
-
-            if (result.data.data.length === 0) {
-              return
-            }
-            setMyUserArtwork(result.data.data)
-            console.log(result.data.data)
-
-          })
-          .catch((error) => console.log(error))
+      bringMyUserArtworks(userCredentialsRdx.credentials.token)
+        .then((result) => {
+          if (result.data.data.length === 0) {
+            return
+          }
+          setMyUserArtwork(result.data.data)
+        })
+        .catch((error) => console.log(error))
     }
   }, [myUserArtwork])
 
-useEffect(() => {
-
-  if (allArtworks.length === 0) {
+  useEffect(() => {
+    if (allArtworks.length === 0) {
       setTimeout(() => {
-        bringAllArtworks()
+        bringAllArtworksAsUser(userCredentialsRdx.credentials.token)
           .then((result) => {
             setLoading(false)
 
@@ -56,116 +55,91 @@ useEffect(() => {
               return
             }
             setAllArtworks(result.data.data)
+
+            const savedArtworks = {}
+            result.data.data.forEach((artwork) => {
+              if (
+                artwork.Users.length > 0 &&
+                artwork.Users[0].User_artworks.favorite === 1
+              ) {
+                savedArtworks[artwork.id] = true
+              } else {
+                savedArtworks[artwork.id] = false
+              }
+            })
+            setIsAlreadySaved(savedArtworks)
           })
           .catch((error) => console.log(error))
       }, 2000)
     }
-}, [allArtworks])
+  }, [allArtworks])
 
-useEffect (()=>{
-  if (myUserArtwork.length === 0) {
-    bringMyUserArtworks(userCredentialsRdx.credentials.token)
-      .then((result) => {
+  console.log(isAlreadySaved[2])
 
-        if (result.data.data.length === 0) {
-          return
-        }
-        setMyUserArtwork(result.data.data)
-        console.log(result.data.data)
-
-      })
-      .catch((error) => console.log(error))
-}
-},[userCredentialsRdx.credentials.token])
-
-
-const artworkSelected = (artwork) => {
+  const artworkSelected = (artwork) => {
     dispatch(addChoosenArtwork({ choosenArtwork: artwork }))
-    console.log(dispatch(addChoosenArtwork({ choosenArtwork: artwork })));
-    console.log(artwork.id)
-  const artworkId = artwork.id
 
-   const isRegistered = myUserArtwork.some(
-    (userArtwork) => userArtwork.artwork_id === artwork.id
-   )
+    setTimeout(() => {
+      navigate('/artwork-details')
+    }, 500)
+  }
 
-   if (isRegistered) {
-    const artworkToUpdateFav = myUserArtwork.find (
+  const addToUserArtwork = (artwork) => {
+    const artworkId = artwork.id
+    console.log(artwork, 'entro addToUserArtwork')
+    const isRegistered = myUserArtwork.some(
       (userArtwork) => userArtwork.artwork_id === artwork.id
     )
-    updateFavorite(
-      artworkToUpdateFav.id,
-      { favorite: !artworkToUpdateFav.favorite },
-      userCredentialsRdx.credentials.token
-    )
-      .then(() => {
-        setMyUserArtwork((prev) =>
-          prev.map((userArtwork) =>
-            userArtwork.id === artworkToUpdateFav.id
-              ? {
-                  ...userArtwork,
-                  favorite: !artworkToUpdateFav.favorite,
-                }
-              : userArtwork
+    console.log(isRegistered, 'isRegistered')
+
+    if (isRegistered) {
+      const artworkToUpdateFav = myUserArtwork.find(
+        (userArtwork) => userArtwork.artwork_id === artwork.id
+      )
+
+      updateFavorite(
+        artworkToUpdateFav.id,
+        { favorite: !artworkToUpdateFav.favorite },
+        userCredentialsRdx.credentials.token
+      )
+        .then(() => {
+          setMyUserArtwork((prevState) =>
+            prevState.map((userArtwork) =>
+              userArtwork.id === artworkToUpdateFav.id
+                ? {
+                    ...userArtwork,
+                    favorite: !artworkToUpdateFav.favorite,
+                  }
+                : userArtwork
+            )
           )
-        );
-      })
-      .catch((error) => console.log(error));
-  } else {
-    addFavorite(artworkId, userCredentialsRdx.credentials.token)
-      .then(() => {
-        setMyUserArtwork((prev) => [...prev, { artwork_id: artworkId, favorite: true }]);
-      })
-      .catch((error) => console.log(error));
+
+          setIsAlreadySaved((prevState) => ({
+            ...prevState,
+            [artworkId]: !artworkToUpdateFav.favorite,
+          }))
+        })
+        .catch((error) => console.log(error))
+    } else {
+      console.log(artworkId, 'entro addfavorite')
+
+      addFavorite(
+        { artwork_id: artworkId, favorite: true },
+        userCredentialsRdx.credentials.token
+      )
+        .then(() => {
+          setMyUserArtwork((prevState) => [
+            ...prevState,
+            { artwork_id: artworkId, favorite: true },
+          ])
+          setIsAlreadySaved((prevState) => ({
+            ...prevState,
+            [artworkId]: true,
+          }))
+        })
+        .catch((error) => console.log(error))
+    }
   }
-};
- 
-    //   setParams(userArtwork.id);
-
-    //   if (artwork.id === userArtwork.artwork_id ) {
-    //     //Register exists then update
-    //     console.log(params);
-    //     console.log("entro porque ids son iguales");
-    //     if (userArtwork.favorite === true) {
-    //       console.log(userArtwork.favorite,"aqui favorite es true");
-    //       setModifyFavorite({
-    //         id: params,
-    //         favorite: false
-    //       })
-    //       console.log(modifyFavorite,"aqui favorite es false");
-
-    //       updateFavorite(params, modifyFavorite, userCredentialsRdx.credentials.token)
-    //       console.log(myUserArtwork,"aqui traigo myUserARtwork actualizado");
-          
-
-    //     } else {
-    //       console.log(userArtwork.favorite,"aqui favorite es false");
-
-    //       setModifyFavorite({
-    //         id: params,
-    //         favorite: true
-    //       })
-    //       console.log(modifyFavorite,"aqui favorite es true");
-
-    //       updateFavorite(params, modifyFavorite, userCredentialsRdx.credentials.token)
-    //     }
-
-    //   } else {
-      
-    //     setCreateFavorite({
-    //       favorite: true,
-    //       artwork_id: artworkId
-    //     })
-    //     addFavorite(createFavorite, userCredentialsRdx.credentials.token)
-    //   }
-    // })
-    
-    // setTimeout(() => {
-    //   navigate('/artwork-details')
-    // }, 1000)
-  
-
- 
 
   if (loading) {
     return (
@@ -188,12 +162,13 @@ const artworkSelected = (artwork) => {
               <div className="cardsContainer">
                 {allArtworks.map((artwork) => {
                   return (
-                    <div 
-                    className="pinDesign " 
-                    onMouseEnter={() => setPostHovered(true)}
-                    onMouseLeave={() => setPostHovered(false)}
-                    onClick={() => artworkSelected(artwork)}
-                    key={artwork.id}>
+                    <div
+                      className="pinDesign "
+                      onMouseEnter={() => setPostHovered(true)}
+                      onMouseLeave={() => setPostHovered(false)}
+                      onClick={() => artworkSelected(artwork)}
+                      key={artwork.id}
+                    >
                       {/* <p>
                       <span className="pe-4 nameFieldDesign">Title:</span>
                       {artwork.title}
@@ -203,28 +178,24 @@ const artworkSelected = (artwork) => {
                         src={`http://localhost:3000/static/${artwork.image_url}`}
                       />
                       {postHovered && (
-                        <div
-                          className=""
-                        >
-                          <div
-                          className=""
-                          >
-                            <div onClick={() => addToUserArtwork()} className="">
-                              hey
-                              {/* <a href=""
-                              onClick={(e) => e.stopPropagation()}></a> */}
-{/* 
-                              {alreadySaved?.length !== 0 ? (
-                                <>
-                                <button>Saved</button>
-                                </>
-                              ): (
-                                <>
-                                <button>Save</button>
-                                </>
-                              )} */}
-                              
-                              
+                        <div className="">
+                          <div className="">
+                            <div className="">
+                              <a href=""></a>
+                              <>
+                                <button
+                                  onClick={(e) => {
+                                    e.stopPropagation()
+                                    addToUserArtwork(artwork)
+                                  }}
+                                  type="button"
+                                  className="bg-danger"
+                                >
+                                  {isAlreadySaved[artwork.id]
+                                    ? 'Saved'
+                                    : 'Save'}
+                                </button>
+                              </>
                             </div>
                           </div>
                         </div>
--- ArtHive project/src/services/apiCalls.js
+++ ArtHive project/src/services/apiCalls.js
@@ -29,6 +29,16 @@ export const bringAllArtworks = async () => {
  
     return await axios.get(`${root}artworks`);
   }
+
+  export const bringAllArtworksAsUser = async (token) => {
+    let config = {
+      headers: { 
+        'Authorization': 'Bearer '+ token,  
+      }
+    };
+
+    return await axios.get(`${root}artworks/user`,config);
+  }
   
 export const bringAllMyArtworks = async (token) => {
   let config = {
