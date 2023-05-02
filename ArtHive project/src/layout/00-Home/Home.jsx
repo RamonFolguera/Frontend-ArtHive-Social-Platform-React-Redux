449574069054f5dc2f61ea4060256be7ad88fd28
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-02 16:29:00 +0200

    fix: worked around some css adjustments for home to center properly the main container

--- ArtHive project/src/layout/00-Home/Home.jsx
+++ ArtHive project/src/layout/00-Home/Home.jsx
@@ -1,67 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import './Home.css'
-import { bringAllArtworks } from "../../services/apiCalls";
-import { NavBar } from "../../components/Navbar/NavBar";
-import community from "../../assets/images/community.png"
+import { bringAllArtworks } from '../../services/apiCalls'
+import { NavBar } from '../../components/Navbar/NavBar'
+import community from '../../assets/images/community.png'
+import { Link } from 'react-router-dom'
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs'
 
 export const Home = () => {
+  const [allArtworks, setAllArtworks] = useState([])
 
-const [allArtworks, setAllArtworks] = useState([]);
-
-
-useEffect(() => {
+  useEffect(() => {
     if (allArtworks.length === 0) {
-        bringAllArtworks()
+      bringAllArtworks()
         .then((result) => {
           if (result.data.data.length === 0) {
-            return;
+            return
           }
-          const randomArtwork = result.data.data[Math.floor(Math.random() * result.data.data.length)];
-          setAllArtworks([randomArtwork]);
+          const randomArtwork =
+            result.data.data[
+              Math.floor(Math.random() * result.data.data.length)
+            ]
+          setAllArtworks([randomArtwork])
         })
-        .catch((error) => console.log(error));
+        .catch((error) => console.log(error))
     }
-  }, []);
+  }, [])
 
+  console.log(allArtworks, 'artwork random')
+  return (
+    <>
+      <NavBar />
+      <Container>
+        <Row className="my-auto homeSection1">
+          <Col lg={6}>
+            <p className="titleDesign">
+              Discover limitless creativity everyday
+            </p>
+            <div className="d-flex justify-content-start align-items-end">
+              <img
+                src={community}
+                alt="Several users profile pictures"
+                style={{ width: 'auto', height: '5em', objectFit: 'contain' }}
+              />
+              <p className="joinTitleDesign d-flex align-items-center ms-4">
+                JOIN OUR 150K+ COMMUNITY OF TALENTED ARTISTS
+              </p>
+            </div>
 
+            <div className="buttonGoToRegisterContainerDesign d-flex justify-content-start">
+              <Link
+                className="buttonGoToRegisterDesign pt-3 pb-3 ps-5 pe-5"
+                to="/register"
+              >
+                GET STARTED <BsFillArrowUpRightCircleFill className="bs" />
+              </Link>
+            </div>
+          </Col>
 
-console.log(allArtworks, "artwork random");
-    return (
-      <>
-        <NavBar/>
-        <Container fluid className="m-0 main-container">
-            <Row className="homeSection1">
-              <Col lg={6} className="ps-5">
-              <p className="titleDesign">Discover limitless creativity everyday</p>
-              <div className="d-flex justify-content-start align-items-end">
-                <img src={community} alt="Several users profile pictures" style={{ width: 'auto', height: '5em', objectFit: 'contain' }} />
-                <p className="joinTitleDesign d-flex align-items-center ms-4">JOIN OUR 150K+ COMMUNITY OF TALENTED ARTISTS</p>
-              </div>
-              </Col>
+          <Col lg={6} className="selectedImgCol">
+            {allArtworks.map((artwork) => {
+              return (
+                <div key={artwork.id}>
+                  <div className="d-flex align-items-end justify-content-between">
+                    <span className="pe-4 nameFieldDesign">Title:</span>
+                    <p>{artwork.title}</p>
 
-            <Col lg={5} className="selectedImgCol" >
-          
-                {allArtworks.map((artwork) => {
-                    return (
-                    <div key={artwork.id}>
-                        <div className="d-flex align-items-end justify-content-between">
-                       
-                            <span className="pe-4 nameFieldDesign">Title:</span>
-                            <p>{artwork.title}</p>
-                        
-                            
-                            <img className="selectedImgDesign" src={`http://localhost:3000/static/${artwork.image_url}`}/>
-                        </div>
-                    </div>
-            );
-          })}
-     
-                </Col>
-                
-            </Row>
-       
-        </Container>
-        </>
-
-    )};
+                    <img
+                      className="selectedImgDesign"
+                      src={`http://localhost:3000/static/${artwork.image_url}`}
+                    />
+                  </div>
+                </div>
+              )
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
