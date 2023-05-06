e4d1e9b5dc6d756057dee28a2addae097d098010
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-05-06 14:59:22 +0200

    feat: added buttons to filter by category. added to render depending of that selectedCategory

--- ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
+++ ArtHive project/src/layout/04-ArtworkGallery/ArtworkGallery.jsx
@@ -17,7 +17,8 @@ import { addChoosenArtwork, artworkData } from '../artworkSlice'
 import { userData } from '../userSlice'
 import { SpinnerComponent } from '../../components/SpinnerComponent/SpinnerComponent'
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs'
-
+import { FooterTemplate } from '../../components/FooterTemplate/FooterTemplate'
+import { Link } from 'react-router-dom'
 
 export const ArtworkGallery = () => {
   const userCredentialsRdx = useSelector(userData)
@@ -27,6 +28,9 @@ export const ArtworkGallery = () => {
   const [hoveredArtworkId, setHoveredArtworkId] = useState(null)
   const [myUserArtwork, setMyUserArtwork] = useState([])
   const [isAlreadySaved, setIsAlreadySaved] = useState({})
+
+  const [selectedCategory, setSelectedCategory] = useState('ALL') // Category selection
+  const [categoryArtworks, setCategoryArtworks] = useState([]) // Artworks of selected category
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -68,16 +72,35 @@ export const ArtworkGallery = () => {
               }
             })
             setIsAlreadySaved(savedArtworks)
+
+            // filtering artworks for category
+            setCategoryArtworks(
+              result.data.data.filter(
+                (artwork) => artwork.category === selectedCategory
+              )
+            )
           })
           .catch((error) => console.log(error))
       }, 2000)
     }
   }, [allArtworks])
 
-  const artworkSelected = (artwork) => {
+  useEffect(() => {
+    // filtering artworks for category
+    setCategoryArtworks(
+      allArtworks.filter((artwork) => artwork.category === selectedCategory)
+    )
+    console.log(
+      selectedCategory,
+      'entro en useEffect cuando selectedCategory cambia'
+    )
+  }, [selectedCategory])
 
-    dispatch(addChoosenArtwork({ choosenArtwork: artwork }));
- 
+  console.log(categoryArtworks, 'category artworks a ver si cambia')
+
+  const artworkSelected = (artwork) => {
+    dispatch(addChoosenArtwork({ choosenArtwork: artwork }))
+
     const isRegistered = myUserArtwork.some(
       (userArtwork) => userArtwork.artwork_id === artwork.id
     )
@@ -86,8 +109,7 @@ export const ArtworkGallery = () => {
       const artworkToUpdateFav = myUserArtwork.find(
         (userArtwork) => userArtwork.artwork_id === artwork.id
       )
-     
-      }
+    }
     setTimeout(() => {
       navigate('/artwork-details')
     }, 500)
@@ -105,7 +127,6 @@ export const ArtworkGallery = () => {
       const artworkToUpdateFav = myUserArtwork.find(
         (userArtwork) => userArtwork.artwork_id === artwork.id
       )
-     
 
       updateFavorite(
         artworkToUpdateFav.id,
@@ -151,71 +172,199 @@ export const ArtworkGallery = () => {
     }
   }
 
+  const handleCategoryClick = (category) => {
+    console.log(category, 'entro handleCategoryClick')
+    setSelectedCategory(category)
+  }
+
   if (loading) {
     return (
-      
-        <div className="spinnerDesign d-flex justify-content-center align-items-center flex-column">
-          <SpinnerComponent message="Art is coming... hold on!" />
-        </div>
+      <div className="spinnerDesign d-flex justify-content-center align-items-center flex-column">
+        <SpinnerComponent message="Art is coming... hold on!" />
+      </div>
     )
   } else if (allArtworks.length > 0) {
     return (
-     
-
+      <>
+        <NavBar />
         <Container fluid>
-          <h2>All artworks:</h2>
+          <h2 className="galleryTitleDesign text-center">Artworks Gallery</h2>
+          <Row>
+            <Col
+              lg={12}
+              className="mt-4 d-flex flex-wrap justify-content-center"
+            >
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('ALL')}
+              >
+                ALL
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Abstract painting')}
+              >
+                ABSTRACT PAINTING
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Oil painting')}
+              >
+                OIL PAINTING
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Photography')}
+              >
+                PHOTOGRAPHY
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Digital painting')}
+              >
+                DIGITAL PAINTING
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Watercolor painting')}
+              >
+                WATERCOLOR PAINTING
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Acrylic painting')}
+              >
+                ACRYLIC PAINTING
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Graffiti Art')}
+              >
+                GRAFFITI
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() => handleCategoryClick('Illustration')}
+              >
+                ILLUSTRATION
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() =>
+                  handleCategoryClick('Mix digital and traditional painting')
+                }
+              >
+                DIGITAL + TRADITIONAL
+              </button>
+              <button
+                className="categoryBtnDesign pt-3 pb-3 ps-5 pe-5 me-3 mt-2"
+                onClick={() =>
+                  handleCategoryClick('Mix photography and digital')
+                }
+              >
+                PHOTOGRAPHY + DIGITAL
+              </button>
+            </Col>
+          </Row>
           <Row className="homeSection1">
-            <Col>
-              <div className="cardsGalleryContainer">
-                {allArtworks.map((artwork) => {
+            {selectedCategory === 'ALL' ? (
+              <Col>
+                <div className="cardsGalleryContainer">
+                  {allArtworks.map((artwork) => {
                     const isHovered = artwork.id === hoveredArtworkId
-                  return (
-                    <div
-                      className="pinDesign "
-                      onMouseEnter={() => setHoveredArtworkId(artwork.id)}
-                      onMouseLeave={() => setHoveredArtworkId(null)}
-                      onClick={() => artworkSelected(artwork)}
-                      key={artwork.id}
-                    >
-                      {/* <p>
+                    return (
+                      <div
+                        className="pinDesign "
+                        onMouseEnter={() => setHoveredArtworkId(artwork.id)}
+                        onMouseLeave={() => setHoveredArtworkId(null)}
+                        onClick={() => artworkSelected(artwork)}
+                        key={artwork.id}
+                      >
+                        {/* <p>
                       <span className="pe-4 nameFieldDesign">Title:</span>
                       {artwork.title}
                     </p> */}
-                      <img
-                        className="imgDesign"
-                        src={`http://localhost:3000/static/${artwork.image_url}`}
-                      />
-                      {isHovered && (
-                      
-                              <>
-                              <button type="button"
-                                  className="detailsBtn"
-                                  onClick={() => artworkSelected(artwork)}>
-                                    <BsFillArrowUpRightCircleFill className="bs"/>  Go to Details
-                              </button>
-                                <button
-                                  onClick={(e) => {
-                                    e.stopPropagation()
-                                    addToUserArtwork(artwork)
-                                  }}
-                                  type="button"
-                                  className="saveBtn"
-                                >
-                                  {isAlreadySaved[artwork.id]
-                                    ? 'Saved'
-                                    : 'Save'}
-                                </button>
-                              </>
-                      
-                      )}
-                    </div>
-                  )
-                })}
-              </div>
-            </Col>
+                        <img
+                          className="imgDesign"
+                          src={`http://localhost:3000/static/${artwork.image_url}`}
+                        />
+                        {isHovered && (
+                          <>
+                            <button
+                              type="button"
+                              className="detailsBtn"
+                              onClick={() => artworkSelected(artwork)}
+                            >
+                              <BsFillArrowUpRightCircleFill className="bs" /> Go
+                              to Details
+                            </button>
+                            <button
+                              onClick={(e) => {
+                                e.stopPropagation()
+                                addToUserArtwork(artwork)
+                              }}
+                              type="button"
+                              className="saveBtn"
+                            >
+                              {isAlreadySaved[artwork.id] ? 'Saved' : 'Save'}
+                            </button>
+                          </>
+                        )}
+                      </div>
+                    )
+                  })}
+                </div>
+              </Col>
+            ) : (
+              <Col>
+    <div className="cardsGalleryContainer">
+      {categoryArtworks
+        .map((artwork) => {
+          const isHovered = artwork.id === hoveredArtworkId
+          return (
+            <div
+              className="pinDesign"
+              onMouseEnter={() => setHoveredArtworkId(artwork.id)}
+              onMouseLeave={() => setHoveredArtworkId(null)}
+              onClick={() => artworkSelected(artwork)}
+              key={artwork.id}
+            >
+              <img
+                className="imgDesign"
+                src={`http://localhost:3000/static/${artwork.image_url}`}
+              />
+              {isHovered && (
+                <>
+                  <button
+                    type="button"
+                    className="detailsBtn"
+                    onClick={() => artworkSelected(artwork)}
+                  >
+                    <BsFillArrowUpRightCircleFill className="bs"/> Go to Details
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      addToUserArtwork(artwork)
+                    }}
+                    type="button"
+                    className="saveBtn"
+                  >
+                    {isAlreadySaved[artwork.id] ? 'Saved' : 'Save'}
+                  </button>
+                </>
+              )}
+            </div>
+          )
+        })}
+    </div>
+  </Col>
+            )}
+            
           </Row>
         </Container>
-   
+        <FooterTemplate />
+      </>
     )
   }
 }
